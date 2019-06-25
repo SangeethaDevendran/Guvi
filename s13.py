4cfa1d356c2f06d4c4590b956b4180cad0924b35
SangeethaDevendran <51980967+SangeethaDevendran@users.noreply.github.com>
2019-06-25 11:11:16 +0530

    Create s13.py

---
+++ s13.py
@@ -0,0 +1,7 @@
+check=input()
+if(check=='A' or check=='a' or check=='E' or check=='e' or check=='I' or check=='i' or check=='O' or check=='o' or check=='U' or check=='u'):
+  print("Vowel")
+elif(check!='A' or check!='a' or check!='E' or check!='e' or check!='I' or check!='i' or check!='O' or check!='o' or check!='U' or check!='u'):
+  print("Consonant")
+else:
+  print("invalid")
