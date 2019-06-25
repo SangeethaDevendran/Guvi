6e43dd8e78448987f9cbd701df47bd48cc3ece86
SangeethaDevendran <51980967+SangeethaDevendran@users.noreply.github.com>
2019-06-25 11:18:08 +0530

    Update s13.py

--- s13.py
+++ s13.py
@@ -1,4 +1,4 @@
-check=input()
+check='a'
 if(check=='A' or check=='a' or check=='E' or check=='e' or check=='I' or check=='i' or check=='O' or check=='o' or check=='U' or check=='u'):
   print("Vowel")
 elif(check!='A' or check!='a' or check!='E' or check!='e' or check!='I' or check!='i' or check!='O' or check!='o' or check!='U' or check!='u'):
