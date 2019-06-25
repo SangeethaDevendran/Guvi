dc63db6a804be13bbe4396c0d3997fd438872fc2
SangeethaDevendran <51980967+SangeethaDevendran@users.noreply.github.com>
2019-06-25 11:58:13 +0530

    Update s15.py

--- s15.py
+++ s15.py
@@ -2,9 +2,9 @@ LAR1=int(input())
 LAR2=int(input())
 LAR3=int(input())
 if(LAR1 > LAR2 and LAR1 > LAR3):
-  Largest=LAR1
+  print(LAR1)
 if(LAR2 > LAR1 and LAR2 > LAR3):
-  Largest=LAR2
+  print(LAR2)
 else:
-  Largest=LAR3
-print(Largest)
+  print(LAR3)
+
