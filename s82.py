20b83e458777735f2f46419019cdf8f354ae4190
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-06-30 21:07:16 +0530

    Create s82.py

---
+++ s82.py
@@ -0,0 +1,8 @@
+cha=input()
+for i in cha:
+  if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U"):
+    print("yes")
+    break
+else:
+  print("no")
+  
